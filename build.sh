8c22698a92a3993844c112bf219ac33724a6c3db
Pavel Turapin <paveltrpn@mail.ru>
2025-06-29 21:21:44 +0400

    update gitignore, make build script command line arguments

--- build.sh
+++ build.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
 SOURCE_DIR="src"
+BUILD_ARTIFACTS_DIR="out"
 
-kotlinc $SOURCE_DIR/Main.kt
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -r|--run)
+      java -cp $BUILD_ARTIFACTS_DIR MainKt
+      shift # past argument
+      shift # past value
+      ;;
+    -c|--compile)
+      kotlinc $SOURCE_DIR/Main.kt -d $BUILD_ARTIFACTS_DIR -verbose
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
