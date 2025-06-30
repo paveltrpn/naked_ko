93fbfcb89216038ba65dd308f63e24f27505cf4a
Pavel Turapin <paveltrpn@integra-s.com>
2025-06-30 16:36:25 +0400

    seperate build

--- _naked_build.sh
+++ _naked_build.sh
@@ -7,6 +7,8 @@ MAIN_PACKAGE="naked"
 
 shopt -s nullglob
 
+# ==============================================
+# === define sources
 TEST_SOURCE=(
   test/src/main/kotlin/Main.kt
   test/src/main/kotlin/Additional.kt
@@ -20,16 +22,22 @@ MODULE_PKG_SOURCE=(
   modules/src/main/kotlin/first/Module.kt
   modules/src/main/kotlin/algebra/mtrx2.kt
 )
+# ==============================================
 
 cleanBuildArtifacts() {
     echo -e "=== clean..."
-    rm -r $BUILD_ARTIFACTS_DIR
+
+    if [[ -d "$BUILD_ARTIFACTS_DIR" ]]; then
+      rm -r $BUILD_ARTIFACTS_DIR
+    fi
 }
 
-if [ ! -d "$BUILD_ARTIFACTS_DIR" ]; then
-  echo -e "=== $BUILD_ARTIFACTS_DIR does not exist. Create one...\n"
-  mkdir $BUILD_ARTIFACTS_DIR
-fi
+checkBuildDir() {
+  if [[ ! -d "$BUILD_ARTIFACTS_DIR" ]]; then
+    echo -e "=== $BUILD_ARTIFACTS_DIR does not exist. Create one...\n"
+    mkdir $BUILD_ARTIFACTS_DIR
+  fi
+}
 
 build() {
     echo -e "=== build..."
@@ -44,48 +52,71 @@ build() {
         -jvm-target $JVM_TARGET \
         -language-version $KOTLIN_VERSION \
         -include-runtime \
-        "${TEST_SOURCE[@]}" \
         "${FIBO_SOURCE[@]}" \
         "${MODULE_PKG_SOURCE[@]}" \
-        -d $BUILD_ARTIFACTS_DIR/a.jar -verbose
+        -d $BUILD_ARTIFACTS_DIR/fibo.jar -verbose
+
+    kotlinc \
+        -jvm-target $JVM_TARGET \
+        -language-version $KOTLIN_VERSION \
+        -include-runtime \
+        "${TEST_SOURCE[@]}" \
+        "${MODULE_PKG_SOURCE[@]}" \
+        -d $BUILD_ARTIFACTS_DIR/test.jar -verbose
 }
 
 run() {
     echo -e "=== run...\n"
-    java -cp $BUILD_ARTIFACTS_DIR/a.jar naked.MainKt
-    java -cp $BUILD_ARTIFACTS_DIR/a.jar fibo.FiboKt
+
+    if [[ ! -d "$BUILD_ARTIFACTS_DIR" ]]; then
+      echo -e "=== $BUILD_ARTIFACTS_DIR does not exist. nothing to run...\n"
+      exit
+    fi
+
+    java -cp $BUILD_ARTIFACTS_DIR/test.jar naked.MainKt
+    java -cp $BUILD_ARTIFACTS_DIR/fibo.jar fibo.FiboKt
 }
 
 while getopts ':-:brac' VAL ; do
   case $VAL in
     b )
+      checkBuildDir
       build
+      exit
       ;;
 #    r ) OFILE="$OPTARG" ;;
     r )
       run
+      exit
       ;;
     a )
       build
       run
+      exit
       ;;
     c )
       cleanBuildArtifacts
+      exit
       ;;
     - )
       case $OPTARG in
         build )
+          checkBuildDir
           build
+          exit
           ;;
         run )
           run
+          exit
           ;;
         all )
           build
           run
+          exit
           ;;
         clean )
           cleanBuildArtifacts
+          exit
           ;;
         * )
           echo "unknown long argument: $OPTARG"
